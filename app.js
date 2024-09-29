d792796803db7a4203c0407a645019059dd0c174
Shivanshu9120 <kshatriyasarkar214@gmail.com>
2024-09-30 01:23:07 +0530

    Final Changes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
     })
 })();
 // type writer
-const textArr = ['Competitive Programmer', 'I solved many DSA problems','Web Developer']; /* The text */
+const textArr = ['Competitive Programmer', 'Web Developer','UI/UX Designer','Machine Learning Enthusiast']; /* The text */
 const speed = 125; /* The speed/duration of the effect in milliseconds */
 let index = 0;
 let textIndex = 0;
